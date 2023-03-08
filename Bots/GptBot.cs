e184c250da4c98fb1671c1bf529f9aa421d2a174
Jordan MÉDINA <jordan.medina@acensi.ca>
2023-03-08 11:56:17 -0500

    Manage context by conversation

--- Bots/GptBot.cs
+++ Bots/GptBot.cs
@@ -9,6 +9,7 @@ using ChatSensei.Models.Client;
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
+using Microsoft.Bot.Schema.Teams;
 using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
 using System.Threading;
@@ -20,12 +21,25 @@ namespace chatSensei.Bots
     {
         private readonly IImageAIClient imageClient;
         private readonly ITextAIClient textClient;
-        private static readonly ChatContext chatContext = new ChatContext();
+        private readonly static Dictionary<string, ChatContext> conversationContext = new Dictionary<string, ChatContext>();
 
         public GptBot(IImageAIClient imageClient, ITextAIClient textClient)
         {
             this.imageClient = imageClient;
             this.textClient = textClient;
+        }
+
+        public string CheckConversationID(ITurnContext<IMessageActivity> turnContext)
+        {
+            string conversationId = turnContext.Activity.Conversation.Id;
+
+            if (!conversationContext.ContainsKey(conversationId))
+            {
+                // Create a new ConversationState for this conversation if one does not already exist
+                conversationContext[conversationId] = new ChatContext();
+            }
+
+            return conversationId;
         }
 
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
@@ -34,22 +48,23 @@ namespace chatSensei.Bots
 
             string userText = turnContext.Activity.RemoveRecipientMention();
 
-            IMessageActivity finalResponse = await GetChatGPTResponseAsync(userText);
+            IMessageActivity finalResponse = await GetChatGPTResponseAsync(userText, turnContext);
             await turnContext.SendActivityAsync(finalResponse, cancellationToken);
         }
 
-        private async Task<IMessageActivity> GetChatGPTResponseAsync(string inputMessage)
+        private async Task<IMessageActivity> GetChatGPTResponseAsync(string inputMessage, ITurnContext<IMessageActivity> turnContext)
         {
-            IMessageActivity responseMessage = null;
             var apiModel = ApiModelFactory.CreateApiModel(inputMessage);
 
+            IMessageActivity responseMessage;
             if (apiModel.Type == ApiModelType.Image)
             {
                 responseMessage = await imageClient.CallOpenAIApi(apiModel as ImageApiModel, inputMessage);
             }
             else
             {
-                responseMessage = await textClient.CallOpenAIApi(apiModel as TextApiModel, chatContext, inputMessage);
+                string conversationID = CheckConversationID(turnContext);
+                responseMessage = await textClient.CallOpenAIApi(apiModel as TextApiModel, conversationContext[conversationID], inputMessage);
             }
 
             return responseMessage;
